643b9dc60ceaec5d953ef49c94a651a1968a0177
vas <vas77794@gmail.com>
2026-02-05 20:03:16 +0100

    amux: state s3s3

--- 4. Projects/2. API-axum/Problem/mySolution/src/handlers/mod.rs
+++ 4. Projects/2. API-axum/Problem/mySolution/src/handlers/mod.rs
@@ -1,70 +1,148 @@
-use crate::models::*;
-use axum::{Json, response::IntoResponse};
+use axum::{extract::State, http::StatusCode, response::IntoResponse, Json};
+
+use crate::{models::*, AppState};
+
 mod handlers_inner;
 
-// ---- CRUD for Questions ----
-
-pub async fn create_question(Json(question): Json<Question>) -> impl IntoResponse {
-    println!("Creating question");
-    let q: Question = question.into();
-
-    Json(QuestionDetail {
-        question_uuid: "uuid".to_owned(),
-        title: q.title.clone(),
-        description: q.description.clone(),
-        created_at: "today".to_owned(),
-    })
+impl IntoResponse for handlers_inner::HandlerError {
+    fn into_response(self) -> axum::response::Response {
+        match self {
+            handlers_inner::HandlerError::BadRequest(msg) => {
+                (StatusCode::BAD_REQUEST, msg).into_response()
+            }
+            handlers_inner::HandlerError::InternalError(msg) => {
+                (StatusCode::INTERNAL_SERVER_ERROR, msg).into_response()
+            }
+        }
+    }
 }
 
-pub async fn read_questions() -> impl IntoResponse {
-    println!("Reading questions");
-    Json(vec![QuestionDetail {
-        question_uuid: "uuid".to_owned(),
-        title: "titel".to_owned(),
-        description: "q.description".to_owned(),
-        created_at: "today".to_owned(),
-    }])
+// // ---- CRUD for Questions ----
+
+pub async fn create_question(
+    State(AppState { questions_dao, .. }): State<AppState>,
+    Json(question): Json<Question>,
+) -> Result<impl IntoResponse,handlers_inner::HandlerError> {
+    // TODO: update return type to be of type `Result`. Both the Ok and Err case should contain `impl IntoResponse`.
+    // TODO: Replace the fake data below with a call to `handlers_inner::create_question`.
+    // Return the result wrapped in JSON in the success case and an `HandlerError` in the error case.
+    // NOTE: `IntoResponse` is implemented for `HandlerError` above.
+   
+    todo!()
+
+   
 }
 
-pub async fn delete_question(Json(question_uuid): Json<QuestionId>) {
-    println!("Deliting question");
-    let q: QuestionId = question_uuid.into();
+pub async fn read_questions(// TODO: add questions_dao from app state as an argument
+    State(AppState { questions_dao, .. }): State<AppState>,
+) -> impl IntoResponse {
+
+    let cq = questions_dao.get_questions().await.expect("remove me later");
+
+    Json(cq)
+}
+
+pub async fn delete_question(
+    // TODO: add questions_dao from app state as an argument
+    State(AppState { questions_dao, .. }): State<AppState>,
+    Json(question_uuid): Json<QuestionId>,
+) {
+    let _r= questions_dao.delete_question(question_uuid.question_uuid).await;
+
 }
 
 // ---- CRUD for Answers ----
 
-// TODO: Create a POST route to /answer which accepts an `Answer` and returns `AnswerDetail` as JSON.
-//       The handler function should be called `create_answer`.
-//
-//       hint: this function should look very similar to the create_question function above
-pub async fn create_answer(Json(answer): Json<Answer>) -> impl IntoResponse {
-    println!("Creating question");
-    let a: Answer = answer.into();
-
-    Json(Answer {
-        question_uuid: "quetion_uuid".to_owned(),
-        content: a.content.clone(),
-    })
+pub async fn create_answer(
+    // Example of how to add state to a route
+    State(AppState { answers_dao, .. }): State<AppState>,
+    Json(answer): Json<Answer>,
+) -> impl IntoResponse {
+    Json(answers_dao.create_answer(answer).await.expect("remove me later"))
 }
 
-// TODO: Create a GET route to /answers which accepts an `QuestionId` and returns a vector of `AnswerDetail` as JSON.
-//       The handler function should be called `read_answers`.
-//
-//       hint: this function should look very similar to the read_questions function above
-pub async fn read_answers() -> impl IntoResponse {
-    Json(vec![AnswerDetail {
-        answer_uuid: "auuid".to_owned(),
-        question_uuid: "quuid".to_owned(),
-        content: "content".to_owned(),
-        created_at: "today".to_owned(),
-    }])
+pub async fn read_answers(
+    // TODO: add answers_dao from app state as an argument
+    State(AppState { answers_dao, .. }): State<AppState>,
+    Json(question_uuid): Json<QuestionId>,
+) -> impl IntoResponse {
+    Json(answers_dao.get_answers(question_uuid.question_uuid).await.expect("remove me later"))
 }
 
-// TODO: Create a DELETE route to /answer which accepts an `AnswerId` and does not return anything.
-//       The handler function should be called `delete_answer`.
-//
-//       hint: this function should look very similar to the delete_question function above
-pub async fn delete_answer(Json(answer_uuid): Json<AnswerId>) {
-    println!("Deliting answer");
-    let a: AnswerId = answer_uuid.into();
+pub async fn delete_answer(
+    // TODO: add answers_dao from app state as an argument
+    State(AppState { answers_dao, .. }): State<AppState>,
+    Json(answer_uuid): Json<AnswerId>,
+) {
+    let r = answers_dao.delete_answer(answer_uuid.answer_uuid).await;
 }
+
+
+// pub async fn read_questions(
+//     State(AppState { questions_dao, .. }): State<AppState>,
+// ) -> impl IntoResponse {
+//     // TODO: update return type to be of type `Result`. Both the Ok and Err case should contain `impl IntoResponse`.
+//     // TODO: Replace the fake data below with a call to `handlers_inner::read_questions`.
+//     // Return the result wrapped in JSON in the success case and an `HandlerError` in the error case.
+//     // NOTE: `IntoResponse` is implemented for `HandlerError` above.
+//     Json(vec![QuestionDetail {
+//         question_uuid: "question_uuid".to_owned(),
+//         title: "title".to_owned(),
+//         description: "description".to_owned(),
+//         created_at: "created_at".to_owned(),
+//     }])
+// }
+
+// pub async fn delete_question(
+//     State(AppState { questions_dao, .. }): State<AppState>,
+//     Json(question_uuid): Json<QuestionId>,
+// ) {
+//     // TODO: update return type to be of type `Result`. Both the Ok and Err case should contain `impl IntoResponse`.
+//     // TODO: Make a call to `handlers_inner::delete_question`.
+//     // Return a unit type in the success case and an `HandlerError` in the error case.
+//     // NOTE: `IntoResponse` is implemented for `HandlerError` above.
+// }
+
+// // ---- CRUD for Answers ----
+
+// pub async fn create_answer(
+//     State(AppState { answers_dao, .. }): State<AppState>,
+//     Json(answer): Json<Answer>,
+// ) -> impl IntoResponse {
+//     // TODO: update return type to be of type `Result`. Both the Ok and Err case should contain `impl IntoResponse`.
+//     // TODO: Replace the fake data below with a call to `handlers_inner::create_answer`.
+//     // Return the result wrapped in JSON in the success case and an `HandlerError` in the error case.
+//     // NOTE: `IntoResponse` is implemented for `HandlerError` above.
+//     Json(AnswerDetail {
+//         answer_uuid: "answer_uuid".to_owned(),
+//         question_uuid: "question_uuid".to_owned(),
+//         content: "content".to_owned(),
+//         created_at: "created_at".to_owned(),
+//     })
+// }
+
+// pub async fn read_answers(
+//     State(AppState { answers_dao, .. }): State<AppState>,
+//     Json(question_uuid): Json<QuestionId>,
+// ) -> impl IntoResponse {
+//     // TODO: update return type to be of type `Result`. Both the Ok and Err case should contain `impl IntoResponse`.
+//     // TODO: Replace the fake data below with a call to `handlers_inner::read_answers`.
+//     // Return the result wrapped in JSON in the success case and an `HandlerError` in the error case.
+//     // NOTE: `IntoResponse` is implemented for `HandlerError` above.
+//     Json(vec![AnswerDetail {
+//         answer_uuid: "answer_uuid".to_owned(),
+//         question_uuid: "question_uuid".to_owned(),
+//         content: "content".to_owned(),
+//         created_at: "created_at".to_owned(),
+//     }])
+// }
+
+// pub async fn delete_answer(
+//     State(AppState { answers_dao, .. }): State<AppState>,
+//     Json(answer_uuid): Json<AnswerId>,
+// ) {
+//     // TODO: update return type to be of type `Result`. Both the Ok and Err case should contain `impl IntoResponse`.
+//     // TODO: Make a call to `handlers_inner::delete_answer`.
+//     // Return a unit type in the success case and an `HandlerError` in the error case.
+//     // NOTE: `IntoResponse` is implemented for `HandlerError` above.
+// }
--- 4. Projects/2. API-axum/Problem/mySolution/src/main.rs
+++ 4. Projects/2. API-axum/Problem/mySolution/src/main.rs
@@ -3,13 +3,18 @@ extern crate log;
 
 extern crate pretty_env_logger;
 
-use axum::{
-    Router,
-    routing::{delete, get, post},
-};
+use std::sync::Arc;
 
+use axum::{
+    routing::{delete, get, post},
+    Router,
+};
 use dotenvy::dotenv;
 
+use persistance::{
+    answers_dao::{AnswersDao, AnswersDaoImpl},
+    questions_dao::{QuestionsDao, QuestionsDaoImpl},
+};
 use sqlx::postgres::PgPoolOptions;
 
 mod handlers;
@@ -17,6 +22,12 @@ mod models;
 mod persistance;
 
 use handlers::*;
+
+#[derive(Clone)]
+pub struct AppState {
+    pub questions_dao: Arc<dyn QuestionsDao + Send + Sync>,
+    pub answers_dao: Arc<dyn AnswersDao + Send + Sync>,
+}
 
 #[tokio::main]
 async fn main() {
@@ -31,14 +42,24 @@ async fn main() {
         .await
         .unwrap();
 
+    let questions_dao = Arc::new(QuestionsDaoImpl::new(pool.clone())); // create a new instance of QuestionsDaoImpl passing in `pool` (use the clone method)
+    let answers_dao = Arc::new(AnswersDaoImpl::new(pool)); // create a new instance of AnswersDaoImpl passing in `pool`
+
+    let app_state = AppState{
+        questions_dao: questions_dao,
+        answers_dao: answers_dao,
+    }; // create a new instance of AppState
+
+
     let app = Router::new()
         .route("/question", post(create_question))
         .route("/questions", get(read_questions))
         .route("/question", delete(delete_question))
         .route("/answer", post(create_answer))
         .route("/answers", get(read_answers))
-        .route("/answer", delete(delete_answer));
-
+        .route("/answer", delete(delete_answer))
+            // The with_state method allows us to add state to the state managed by this instance of Axum. Then we can use this state in the handlers.
+        .with_state(app_state); // pass in `app_state` as application state.
     let listener = tokio::net::TcpListener::bind("127.0.0.1:8000")
         .await
         .unwrap();
